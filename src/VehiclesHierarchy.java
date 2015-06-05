91dba83412742a9f535db935c9d0636b196139ee
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-06-06 02:25:48 +0300

    Initial commit

---
+++ src/VehiclesHierarchy.java
@@ -0,0 +1,50 @@
+public class VehiclesHierarchy {
+
+  public static class Vehicle {
+    String name;
+    int numberOfWheels;
+  }
+
+  public interface VehicleInterface {
+    void getWheels();
+  }
+
+  public static class Car extends Vehicle implements VehicleInterface {
+
+    Car(String name) {
+      this.name = name;
+      numberOfWheels = 4;
+      System.out.println("Created a new vehicle: Car.");
+    }
+
+    @Override
+    public void getWheels() {
+      System.out.println("Car \"" + name + "\" has " + numberOfWheels + " wheels.");
+    }
+  }
+
+  public static class Hydroplane extends Vehicle implements VehicleInterface {
+
+    Hydroplane(String name) {
+      this.name = name;
+      numberOfWheels = 0;
+      System.out.println("Created a new vehicle: Hydroplane.");
+    }
+
+    @Override
+    public void getWheels() {
+      System.out.println("Hydroplane \"" + name + "\" has " + numberOfWheels + " wheels.");
+    }
+  }
+
+  public static void main(String[] args) {
+
+    Car car = new Car("Tesla");
+    Hydroplane hydroplane = new Hydroplane("PowerBoat");
+
+    car.getWheels();
+    hydroplane.getWheels();
+  }
+}
+
+
